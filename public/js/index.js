0ce45f04b72c1bc6674e00cb0b0a708020c54f3e
BP Mishra <tech@golivelabs.io>
2015-08-05 11:24:45 -0700

    get and post commands

--- public/js/index.js
+++ public/js/index.js
@@ -7,8 +7,56 @@ console.log('Hello World!');
 /**
  * @function ajax
  * @description Make an ajax call
- * @param url String Url string for the ajax call
+ * @param verb {string} One of the common verbs --> GET, POST, PUT or DELETE
+ * @param url {string} Url string for the ajax call
+ * @param data {object} Data that should be sent along with the request (GET --> convert the object to url encoded)
+ * @param success {function} Callback to be executed if the call is successful
+ * @param error {function} Callback to be executed if the call is unsuccessful
  * */
-function ajax() {
+function ajax(verb, url, data, success, error) {
+
+    verb = verb.toLowerCase().trim();
     // write an XMLHttpRequest
+    var xhr = new XMLHttpRequest();
+    if (verb === 'get' && typeof data !== 'undefined') {
+        // generate the query string and attach it to the url
+        // { name: 'Charles Park' } --> ?name=Charles%20Park
+        var arr = [];
+        for (var prop in data) {
+            arr.push(encodeURIComponent(prop) + '=' + encodeURIComponent(data[prop]));
+        }
+        console.log(arr);
+        url += '?' + arr.join('&');
+        console.log(url);
+    }
+    xhr.open(verb, url);
+    if (/put|post/gmi.test(verb)) {
+        xhr.setRequestHeader('Content-Type', 'application/json');
+    }
+    xhr.addEventListener('readystatechange', function () {
+        if (xhr.status === 200 && xhr.readyState === 4) {
+            success(xhr.responseText);
+        }
+    });
+    if (/put|post/gmi.test(verb) && typeof data !== 'undefined') {
+        xhr.send(JSON.stringify(data));
+    } else {
+        xhr.send();
+    }
 }
+
+ajax('GET', '/test', {
+    name: 'Charles Park',
+    id: 1000,
+    'type of dev': 'UI'
+}, function (response) {
+    console.log(response);
+});
+
+ajax('POST', '/test', {
+    name: 'Charles Park',
+    id: 1000,
+    'type of dev': 'UI'
+}, function (response) {
+    console.log(response);
+});
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/test', function (req, res) {
+    console.log('GET request ... ');
+    console.warn(req.query); // localhost:3000/test?name=charles --> { name: 'Charles' }
+    // console.warn(req.params); // /test/:name --> localhost:3000/test/Charles --> { name: 'Charles' }
+    res.status(200).json({ message: 'IMPL_101' });
+});
+
+router.post('/test', function (req, res) {
+    console.log('POST request ... ');
+    console.log(req.body);
+    res.status(200).json({ message: 'IMPL_101' });
+});
+
 module.exports = router;
